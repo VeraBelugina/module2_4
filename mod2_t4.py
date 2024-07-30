14194c7f42e963cbcdff3d882d5eb4a41c3b1366
VeraBelugina <vryshanchik-cherry@mail.ru>
2024-07-30 18:16:17 +0300

    Update mod2_t4.py

--- mod2_t4.py
+++ mod2_t4.py
@@ -4,9 +4,9 @@ def get_matrix(n, m, value):
         matrix.append([])
         for j in range(m):
             matrix[i].append(value)
-    print(matrix)
+    return matrix
 
 
-get_matrix(2, 2, 10)
-get_matrix(3, 5, 42)
-get_matrix(4, 2, 13)
+print(get_matrix(2, 2, 10))
+print(get_matrix(3, 5, 42))
+print(get_matrix(4, 2, 13))
